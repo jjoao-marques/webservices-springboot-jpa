158131bece87df7052bd6f4f2f3be196004926ff
João Marques <joaomarques.jm991@gmail.com>
2021-04-23 00:23:05 -0300

    Service layer, component registration

--- src/main/java/com/devsuperior/course/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/course/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.course.entities.User;
 
+
 public interface UserRepository  extends JpaRepository<User, Long>{}
