fee15d1602641bda558e4bca1d9abd67b390105f
João Marques <joaomarques.jm991@gmail.com>
2021-04-24 01:14:21 -0300

    Enum OrderStatus

--- src/main/java/com/devsuperior/course/config/TestConfig.java
+++ src/main/java/com/devsuperior/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.devsuperior.course.entities.Order;
 import com.devsuperior.course.entities.User;
+import com.devsuperior.course.entities.enums.OrderStatus;
 import com.devsuperior.course.repositories.OrderRepository;
 import com.devsuperior.course.repositories.UserRepository;
 
@@ -32,9 +33,9 @@ public class TestConfig implements CommandLineRunner {
 		User u3 = new User(null, "Leonidas","leonidas@gmail.com", "16997079978", "010203");
 		
 		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u3);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u3);
 		 
 		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
--- src/main/java/com/devsuperior/course/entities/Order.java
+++ src/main/java/com/devsuperior/course/entities/Order.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.devsuperior.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
@@ -31,16 +32,29 @@ public class Order implements Serializable{
 	@JoinColumn(name = "client_id")
 	private User client;
 	
+	private Integer orderStatus;
+	
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
 	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}	
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -89,10 +103,5 @@ public class Order implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
 	
 }
