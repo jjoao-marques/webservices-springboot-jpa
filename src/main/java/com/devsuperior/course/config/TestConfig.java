65eebdd04ecc04a22f6def1658b73150348d3cd6
João Marques <joaomarques.jm991@gmail.com>
2021-04-26 20:42:21 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/devsuperior/course/config/TestConfig.java
+++ src/main/java/com/devsuperior/course/config/TestConfig.java
@@ -1,6 +1,5 @@
 package com.devsuperior.course.config;
 
-
 import java.time.Instant;
 import java.util.Arrays;
 
@@ -11,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.devsuperior.course.entities.Category;
 import com.devsuperior.course.entities.Order;
+import com.devsuperior.course.entities.OrderItem;
 import com.devsuperior.course.entities.Product;
 import com.devsuperior.course.entities.User;
 import com.devsuperior.course.entities.enums.OrderStatus;
 import com.devsuperior.course.repositories.CategoryRepository;
+import com.devsuperior.course.repositories.OrderItemRepository;
 import com.devsuperior.course.repositories.OrderRepository;
 import com.devsuperior.course.repositories.ProductRepository;
 import com.devsuperior.course.repositories.UserRepository;
@@ -22,52 +23,60 @@ import com.devsuperior.course.repositories.UserRepository;
 @Configuration
 @Profile("test")
 public class TestConfig implements CommandLineRunner {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository producRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
-		
-		User u1 = new User(null, "João Marques", "joao@gmail.com", "16997990177", "123456") ;
-		User u2 = new User(null, "Taiane","taiane@gmail.com", "16988445818", "12345678");
-		User u3 = new User(null, "Leonidas","leonidas@gmail.com", "16997079978", "010203");
-		
-		
+
+		User u1 = new User(null, "João Marques", "joao@gmail.com", "16997990177", "123456");
+		User u2 = new User(null, "Taiane", "taiane@gmail.com", "16988445818", "12345678");
+		User u3 = new User(null, "Leonidas", "leonidas@gmail.com", "16997079978", "010203");
+
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.DELIVERED, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u3);
-		
+
 		Category cat1 = new Category(null, "Eletrônicos");
 		Category cat2 = new Category(null, "Livros");
 		Category cat3 = new Category(null, "Computadores");
-		
+
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
+
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		producRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
-	
-	
+
 }
